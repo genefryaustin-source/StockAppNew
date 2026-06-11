d8b9fc33c0e8c90975240d22119e34b0256d4e3a
genefryaustin-source <genefryaustin@gmail.com>
2026-06-10 21:35:19 -0700

    Update App with more Help Section Detail

--- app.py
+++ app.py
@@ -940,8 +940,8 @@ elif page == "Crypto":
         st.exception(e)
 
 elif page == "Help":
-    from modules.help.help_home import render_help_home
-    render_help_home()
+    from modules.help.help_ui import render_help
+    render_help()
 
 
 
